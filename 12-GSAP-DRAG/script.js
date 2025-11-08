5f60ad160e536eb280ed814974b1d8c5e246758c
Krunal022 <krunalvaghela723@gmail.com>
2025-11-08 22:14:46 +0530

    refine GSAP motion smooth move & Y-axis & rotation

--- 12-GSAP-DRAG/script.js
+++ 12-GSAP-DRAG/script.js
@@ -2,9 +2,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(Draggable, InertiaPlugin)
 
     Draggable.create(".box", {
-        type: "x",
+        type: "x,y",
+        bounds: ".left"
     });
+
     Draggable.create(".cir", {
-        type: "x",
+        type: "x,y",
+        inertia: true,
+        bounds: ".right"
     });
+
+    Draggable.create(".star", {
+        type: "rotation",
+        inertia: true,
+        bounds: ".right"
+    });
+
+    Draggable.create(".hexagon", {
+        type: "y",
+        inertia: true,
+        bounds: ".left"
+    });
+
 });
